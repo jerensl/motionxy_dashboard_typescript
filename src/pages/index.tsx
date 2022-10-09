cfe42da08606626dc8e1d3a218ca4a6fdb16a351
jerensl <54782057+jerensl@users.noreply.github.com>
2022-10-10 02:10:06 +0800

    feature: adding navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { signOut } from 'firebase/auth'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
+import { Navbar } from '../components/Navbar'
 import { useAuth } from '../context/useAuth'
 import { auth } from '../utils/firebase'
 
@@ -19,8 +20,12 @@ const Home: NextPage = () => {
                 <title>Ovord 2</title>
                 <meta name="description" content="Ovord 2 Dashboard" />
             </Head>
+            <Navbar />
             <h1>Welcome</h1>{' '}
-            <button onClick={() => signOut(auth).then(() => router.push('/'))}>
+            <button
+                onClick={() => signOut(auth).then(() => router.push('/'))}
+                className="border border-indigo-500 bg-indigo-500 text-white rounded-md px-4 py-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline"
+            >
                 Logout
             </button>
         </>
