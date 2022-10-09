b1647ee34c6e12bbea4619f5c93cebe71fc984f9
jerensl <54782057+jerensl@users.noreply.github.com>
2022-10-10 02:50:38 +0800

    fix: fix the broken build

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -2,7 +2,7 @@ import * as firebaseApp from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY + '',
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
 }
