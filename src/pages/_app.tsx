cd8bf951a88dbf0f5caeadfb7f6d2d8c93258125
jerensl <54782057+jerensl@users.noreply.github.com>
2022-10-10 19:11:18 +0800

    feature: adding layout and put the navbar in layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AuthProvider } from '../context/useAuth'
+import React from 'react'
+import { NextPage } from 'next'
 
-function MyApp({ Component, pageProps }: AppProps) {
-    return (
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+    getLayout?: (page: React.ReactElement) => React.ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+    Component: NextPageWithLayout
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+    const getLayout = Component.getLayout ?? ((page) => page)
+
+    return getLayout(
         <AuthProvider>
             <Component {...pageProps} />
         </AuthProvider>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
-import { signOut } from 'firebase/auth'
-import type { NextPage } from 'next'
 import Head from 'next/head'
-import { useRouter } from 'next/router'
+import Layout from '../components/Layout'
 import { Navbar } from '../components/Navbar'
 import { useAuth } from '../context/useAuth'
-import auth from '../utils/firebase'
+import { NextPageWithLayout } from './_app'
 
-const Home: NextPage = () => {
-    const router = useRouter()
+const Home: NextPageWithLayout = () => {
     const { user, loading } = useAuth()
 
     if (!user || loading) {
@@ -20,16 +17,12 @@ const Home: NextPage = () => {
                 <title>Ovord 2</title>
                 <meta name="description" content="Ovord 2 Dashboard" />
             </Head>
-            <Navbar />
-            <h1>Welcome</h1>{' '}
-            <button
-                onClick={() => signOut(auth).then(() => router.push('/'))}
-                className="border border-indigo-500 bg-indigo-500 text-white rounded-md px-4 py-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline"
-            >
-                Logout
-            </button>
         </>
     )
+}
+
+Home.getLayout = function getLayout(page: React.ReactElement) {
+    return <Layout>{page}</Layout>
 }
 
 export default Home
