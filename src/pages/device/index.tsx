856024b6fe26d46c7b69e2ea6edfe2b2754c1a67
jerensl <54782057+jerensl@users.noreply.github.com>
2022-11-01 21:40:07 +0800

    feat: add new device modal

--- src/pages/device/index.tsx
+++ src/pages/device/index.tsx
@@ -1,15 +1,38 @@
 import Head from 'next/head'
+import React from 'react'
 import Layout from '../../components/Layout'
+import NewDeviceModal from '../../components/Modal/NewDevice.modal'
 import { NextPageWithLayout } from '../_app'
 
 const DevicePage: NextPageWithLayout = () => {
+    const [newDeviceOpen, setNewDeviceOpen] = React.useState(false)
+
+    const handleNewDeviceOpen = () => {
+        setNewDeviceOpen(true)
+    }
+
+    const handleNewDeviceClosed = () => {
+        setNewDeviceOpen(false)
+    }
+
     return (
         <>
             <Head>
                 <title>Ovord 2</title>
                 <meta name="description" content="Ovord 2 Dashboard" />
             </Head>
-            <div className="ml-80 mt-10 max-w-4xl"></div>
+            <div className="flex flex-row ml-80 mt-10 max-w-full px-10">
+                <button
+                    onClick={handleNewDeviceOpen}
+                    className="m-auto text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/30 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2 "
+                >
+                    Add new device
+                </button>
+                <NewDeviceModal
+                    isOpen={newDeviceOpen}
+                    handleClose={handleNewDeviceClosed}
+                />
+            </div>
         </>
     )
 }
