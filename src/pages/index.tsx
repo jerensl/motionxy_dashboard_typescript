f7126f59e79006cac49ba073db40cc450cf2edab
jerensl <54782057+jerensl@users.noreply.github.com>
2022-11-02 09:36:21 +0800

    feat: add logo and brand

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,8 @@ const Home: NextPageWithLayout = () => {
             <Head>
                 <title>Ovord 2</title>
                 <meta name="description" content="Ovord 2 Dashboard" />
+                <link rel="manifest" href="/manifest.json" />
+                <meta name="theme-color" content="#90cdf4" />
             </Head>
             <div className="ml-80 mt-10 max-w-4xl">
                 <Chart />
