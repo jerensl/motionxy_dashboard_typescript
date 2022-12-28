442680c862cf2dea914621e9a51e35ca87972033
jerensl <54782057+jerensl@users.noreply.github.com>
2022-12-28 12:04:07 +0800

    feat: stat loading center

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -73,7 +73,7 @@ const Home: NextPageWithLayout = () => {
                         viewBox="0 0 24 24"
                         strokeWidth={1.5}
                         stroke="currentColor"
-                        className="m-auto w-8 h-8 m-auto"
+                        className="w-8 h-8 m-auto"
                     >
                         <path
                             strokeLinecap="round"
@@ -85,7 +85,7 @@ const Home: NextPageWithLayout = () => {
                     {statsIsLoading ? (
                         <svg
                             aria-hidden="true"
-                            className="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                            className="m-auto mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
                             viewBox="0 0 100 101"
                             fill="none"
                             xmlns="http://www.w3.org/2000/svg"
