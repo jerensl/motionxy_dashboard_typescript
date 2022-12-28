abf1f08b88d79a99abc12930fc20f0bf25b99ef6
jerensl <54782057+jerensl@users.noreply.github.com>
2022-12-28 12:10:54 +0800

    feat: stat loading center

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,7 +43,7 @@ const Home: NextPageWithLayout = () => {
                     {statsIsLoading ? (
                         <svg
                             aria-hidden="true"
-                            className="m-auto mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                            className="mx-auto w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
                             viewBox="0 0 100 101"
                             fill="none"
                             xmlns="http://www.w3.org/2000/svg"
@@ -85,7 +85,7 @@ const Home: NextPageWithLayout = () => {
                     {statsIsLoading ? (
                         <svg
                             aria-hidden="true"
-                            className="m-auto mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                            className="mx-auto w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
                             viewBox="0 0 100 101"
                             fill="none"
                             xmlns="http://www.w3.org/2000/svg"
