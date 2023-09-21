1e0c615273aa6009ecd3486935a24e010d2226b7
jerensl <54782057+jerensl@users.noreply.github.com>
2023-09-22 06:21:21 +0800

    docs(login): add login docs on storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,6 +7,17 @@ const config: StorybookConfig = {
         '@storybook/addon-essentials',
         '@storybook/addon-onboarding',
         '@storybook/addon-interactions',
+        '@storybook/addon-mdx-gfm',
+        {
+            name: '@storybook/addon-styling',
+            options: {
+                // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
+                // For more details on this addon's options.
+                postCss: {
+                    implementation: require.resolve('postcss'),
+                },
+            },
+        },
     ],
     framework: {
         name: '@storybook/nextjs',
---
+++ src/stories/components/Login.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Login } from '../../components/Login'
+
+const meta: Meta<typeof Login> = {
+    title: 'components/Login',
+    component: Login,
+}
+
+export default meta
+type Story = StoryObj<typeof Login>
+
+export const Basic: Story = {
+    args: {},
+    decorators: [],
+}
