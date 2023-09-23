236bd0391abdc901fa4d5640159287e2624fb44a
jerensl <54782057+jerensl@users.noreply.github.com>
2023-09-23 14:13:44 +0800

    docs(storybook): add storybook theme and remove unnecessary file

---
+++ .storybook/MainTheme.ts
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming/create'
+
+export default create({
+    base: 'light',
+    brandTitle: 'MotionXY',
+    brandUrl: 'https://jerensl.com',
+    brandTarget: '_self',
+})
---
+++ .storybook/manager.ts
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/manager-api'
+import mainTheme from './MainTheme'
+
+addons.setConfig({
+    theme: mainTheme,
+})
